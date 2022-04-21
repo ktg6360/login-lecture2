1b3dc71f5b0ce846d8f2b482191de8f76a976ab3
Chaster Kim <chaster@bzznbyd.com>
2022-04-21 11:04:08 +0900

    fs모듈을 통한 회원가입 기능 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -3,16 +3,21 @@
 const fs = require("fs").promises;
 
 class UserStorage {
-  static getUsers(...fields) {
-    // const users = this.#users;
-    const newUsers = fields.reduce((newUsers, field) => {
-      if (users.hasOwnProperty(field)) {
-        newUsers[field] = users[field];
-      }
-      return newUsers;
-    }, {});
+  static getUsers(isAll, ...fields) {
+    return fs.readFile("./src/databases/users.json")
+      .then((data) => {
+        const users = JSON.parse(data);
+        if (isAll) return users;
+        const newUsers = fields.reduce((newUsers, field) => {
+          if (users.hasOwnProperty(field)) {
+            newUsers[field] = users[field];
+          }
+          return newUsers;
+        }, {});
 
-    return newUsers;
+        return newUsers;
+      })
+      .catch(console.error);
   }
 
   static getUserInfo(id) {
@@ -31,12 +36,16 @@ class UserStorage {
       .catch(console.error);
   }
 
-  static save(userInfo) {
-    // const users = this.#users;
+  static async save(userInfo) {
+    const users = await this.getUsers(true);
+    if (users.id.includes(userInfo.id)) {
+      throw "이미 존재하는 아이디입니다.";
+    }
     users.id.push(userInfo.id);
     users.name.push(userInfo.name);
     users.psword.push(userInfo.psword);
-    return { success: true };
+    fs.writeFile("./src/databases/users.json", JSON.stringify(users));
+    return { success: true }
   }
 };
 
